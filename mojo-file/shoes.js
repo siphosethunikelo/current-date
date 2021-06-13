0d814c41c429a57f68dfb8d670d51f3a0a7fea7a
siphosethunikelo <siphosethunikelo@gmail.com>
2021-06-13 15:03:03 +0200

    current-date

---
+++ mojo-file/shoes.js
@@ -0,0 +1,37 @@
+/**function getCurrentTime(){
+    var theAM_PM;
+    var theDate = NewDate;
+    var theHour = theDate.getHour();
+  
+    alert('before'+ theAm_Pm());
+    
+    if(theHour<12){
+      theAm_Pm = "AM";
+    }
+  else
+  
+    theAM_PM = "PM";
+  }
+    alert('after' + theAM_PM());
+    if(theHour == 0){
+    
+  };
+  if(theHour = 12){
+    the
+  }
+  if(theHour <12){
+    theHour = theHour - 12;
+  }
+   Minutes = getDate.getMinutes();
+  theMinutes = theMinutes + "";
+  if(theMinutes <10){
+    theMinutes = "0" + theMinutes;
+  }
+  var thePullTime = theHour + "," + theMinutes + "" + theAM_PM;
+  document.theform.getCurrentTime.value = thePullTime;**/
+
+    var getCurrentDate = new Date();
+    document.getElementById("p1").innerHTML = Date ("");
+    document.getElementById("p2").innerHTML = CurrentDate("4 april 2021");
+    
+  
